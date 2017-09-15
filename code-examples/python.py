2c0d1e580915130623b5938700bcdaba903c5903
Marc Mims <marc@questright.com>
2017-09-15 10:25:29 -0700

    Support both python2 and python3
    Address issues raised by @b4hand in PR review.

--- code-examples/python.py
+++ code-examples/python.py
@@ -11,15 +11,23 @@ Supported by envision.ai
 from hashlib import sha1
 from time import time
 import base64
-import urllib.request
-import urllib.parse
-import urllib.error
 import json
 import hmac
+import os
+
+try:
+    from urllib2 import (Request, urlopen)
+except ImportError:
+    from urllib.request import (Request, urlopen)
+
+try:
+    from urllib import quote_plus
+except ImportError:
+    from urllib.parse import quote_plus
 
 #Initializations
-follower_wonk_access_id_str = "your-access-id"
-follower_wonk_secret_key_str = "your-secret key"
+follower_wonk_access_id_str = os.environ['SOCIAL_AUTHORITY_ACCESS_ID']
+follower_wonk_secret_key_str = os.environ['SOCIAL_AUTHORITY_SECRET_KEY']
 
 #Class for access FollowerWonk API
 class FollowerWonk(object):
@@ -29,27 +37,21 @@ class FollowerWonk(object):
 
         datime = int(time() + 500)
 
-
-        #Add BMM: Trying to binary format correct
-        keyBin = bytes(follower_wonk_secret_key_str, 'UTF-8')
+        keyBin = follower_wonk_secret_key_str.encode('UTF-8')
         messageStr = "%s\n%s" % (follower_wonk_access_id_str, datime)
-        messageBin = bytes(messageStr, 'UTF-8')
-
+        messageBin = messageStr.encode('UTF-8')
 
         s = hmac.new(keyBin, messageBin, sha1).digest()
         b64 = base64.b64encode(s)
-        signature = urllib.parse.quote_plus(b64)
+        signature = quote_plus(b64)
 
         auth = "AccessID=%s;Timestamp=%s;Signature=%s;" % (follower_wonk_access_id_str, datime, signature)
 
-        req = urllib.request.Request("%s?screen_name=%s;%s" % (uri, username, auth))
-        r = urllib.request.urlopen(req)
+        req = Request("%s?screen_name=%s;%s" % (uri, username, auth))
+        r = urlopen(req)
 
         responseStr = r.read().decode("utf-8")
-        #print("responseStr = ", responseStr)
         response_Json = json.loads(responseStr)
-
-        #print("response_Json = ", response_Json)
 
         r.close()
 
@@ -57,16 +59,9 @@ class FollowerWonk(object):
             return -1
 
         return float(response_Json['_embedded'][0]['social_authority'])
-        #return response_Json
 
-assert FollowerWonk.social_authority("bryanminorphd") > 0
-assert FollowerWonk.social_authority("UserNameDoesNotExist123") == -1
-
-#Test cases
-FollowerWonk.social_authority("bryanminorphd")
-
-FollowerWonk.social_authority("envision_ai")
-
-FollowerWonk.social_authority("google")
-
-
+# Usage examples:
+#
+# FollowerWonk.social_authority("bryanminorphd")
+# FollowerWonk.social_authority("envision_ai")
+# FollowerWonk.social_authority("google")
