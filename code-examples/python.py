f19d691b253cacc0260d11cb90a0221906c21e2c
Marc Mims <marc@questright.com>
2017-09-14 21:08:23 -0700

    Update contributed python example
    This updated python code was submitted via issue #7. Line endings
    changed to Unix to match the existing format. Otherwise, unmodified and
    untested.

--- code-examples/python.py
+++ code-examples/python.py
@@ -1,21 +1,27 @@
+"""
+author: Evan Duffield <eduffield@iacquire.com>
+Thank you to iAcquire for sponsoring development of this module.
+
+Updated by Bryan Minor, Ph.D.
+--> Now works for Python 3
+Supported by envision.ai
+"""
+
+# External libraries
 from hashlib import sha1
 from time import time
 import base64
-import urllib2
-import urllib
+import urllib.request
+import urllib.parse
+import urllib.error
 import json
 import hmac
 
-"""
-author: Evan Duffield <eduffield@iacquire.com>
+#Initializations
+follower_wonk_access_id_str = "your-access-id"
+follower_wonk_secret_key_str = "your-secret key"
 
-Thank you to iAcquire for sponsoring development of this module.
-"""
-
-follower_wonk_access_id = ""
-follower_wonk_secret_key = ""
-
-
+#Class for access FollowerWonk API
 class FollowerWonk(object):
     @staticmethod
     def social_authority(username):
@@ -23,23 +29,44 @@ class FollowerWonk(object):
 
         datime = int(time() + 500)
 
-        s = hmac.new(follower_wonk_secret_key, "%s\n%s" % (follower_wonk_access_id, datime), sha1).digest()
+
+        #Add BMM: Trying to binary format correct
+        keyBin = bytes(follower_wonk_secret_key_str, 'UTF-8')
+        messageStr = "%s\n%s" % (follower_wonk_access_id_str, datime)
+        messageBin = bytes(messageStr, 'UTF-8')
+
+
+        s = hmac.new(keyBin, messageBin, sha1).digest()
         b64 = base64.b64encode(s)
-        signature = urllib.quote_plus(b64)
+        signature = urllib.parse.quote_plus(b64)
 
-        auth = "AccessID=%s;Timestamp=%s;Signature=%s;" % (follower_wonk_access_id, datime, signature)
+        auth = "AccessID=%s;Timestamp=%s;Signature=%s;" % (follower_wonk_access_id_str, datime, signature)
 
-        req = urllib2.Request("%s?screen_name=%s;%s" % (uri, username, auth))
-        r = urllib2.urlopen(req)
+        req = urllib.request.Request("%s?screen_name=%s;%s" % (uri, username, auth))
+        r = urllib.request.urlopen(req)
 
-        response = json.loads(r.read())
+        responseStr = r.read().decode("utf-8")
+        #print("responseStr = ", responseStr)
+        response_Json = json.loads(responseStr)
+
+        #print("response_Json = ", response_Json)
 
         r.close()
 
-        if '_embedded' not in response:
+        if '_embedded' not in response_Json:
             return -1
 
-        return response['_embedded'][0]['social_authority']
+        return float(response_Json['_embedded'][0]['social_authority'])
+        #return response_Json
 
-assert FollowerWonk.social_authority("randfish") > 0
-assert FollowerWonk.social_authority("usernamedoesnotexist123") == -1
+assert FollowerWonk.social_authority("bryanminorphd") > 0
+assert FollowerWonk.social_authority("UserNameDoesNotExist123") == -1
+
+#Test cases
+FollowerWonk.social_authority("bryanminorphd")
+
+FollowerWonk.social_authority("envision_ai")
+
+FollowerWonk.social_authority("google")
+
+
